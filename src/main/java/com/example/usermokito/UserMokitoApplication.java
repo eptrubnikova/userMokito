5498a8e05982d7133458545dcf97815e689b0d8d
eptrubnikova <eptrubnikova@gmail.com>
2023-04-05 14:30:31 +0600

    Initial commit

---
+++ src/main/java/com/example/usermokito/UserMokitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermokito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMokitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMokitoApplication.class, args);
+    }
+
+}
